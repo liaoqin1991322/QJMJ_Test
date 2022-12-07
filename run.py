fa1c8cbf54ce459828dd3a5867d68f519ed3c012
liaoqin96 <403483118@qq.com>
2022-12-07 17:30:03 +0800

    运行

--- run.py
+++ run.py
@@ -9,13 +9,13 @@ import pytest
 
 # pytest.main()
 pytest.main([
-            # 'testcases/test_01register.py',
-            #  'testcases/test_02login.py',
+            'testcases/test_01register.py',
+            'testcases/test_02login.py',
             #  'testcases/test_03recharge.py',
             #  'testcases/test_04withdraw.py',
             #  'testcases/test_05add_audit.py',
             #  'testcases/test_06audit.py',
-             'testcases/test_07invest.py',
+            # 'testcases/test_07invest.py',
              '--reruns','3', #重运行3次
              '--reruns-delay','3', #每3s运行一次
              '--alluredir=allureReports'
